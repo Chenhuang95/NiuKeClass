e0a12901ed7cf3010093105ae4b95cb07c06e60a
HC <329806042@qq.com>
2019-03-26 20:07:35 +0800

    牛客1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 public class Main {
     public static void print(int index,Object obj)
@@ -12,5 +13,8 @@ public class Main {
         Date date =new Date();
         DateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         print(1,dt.format(date));
+
+        Random rd = new Random();
+        print(2,rd.nextInt(800));
     }
 }
