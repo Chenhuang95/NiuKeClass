1d61db05b43b9798db752f5833f8951b8e4faa99
HC <329806042@qq.com>
2019-03-26 18:23:36 +0800

    牛客1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void print(int index,Object obj)
+    {
+        System.out.println(String.format("{%d}, %s",index,obj));
+    }
+    public static void main(String[] args)
+    {
+        Date date =new Date();
+        DateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        print(1,dt.format(date));
+    }
+}
